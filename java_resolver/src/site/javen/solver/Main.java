1a5ecc5b518254850ad3801b0d4373a4286bdeae
coder <zdj@youloft.com>
2021-09-15 19:21:36 +0800

    add http post

--- java_resolver/src/site/javen/solver/Main.java
+++ java_resolver/src/site/javen/solver/Main.java
@@ -1,6 +1,13 @@
 package site.javen.solver;
 
 
+import org.apache.hc.client5.http.async.methods.SimpleHttpRequest;
+import org.apache.hc.client5.http.async.methods.SimpleHttpResponse;
+import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
+import org.apache.hc.client5.http.impl.async.HttpAsyncClients;
+import org.apache.hc.core5.concurrent.FutureCallback;
+import org.apache.hc.core5.http.ContentType;
+
 import java.io.File;
 import java.io.RandomAccessFile;
 import java.math.BigInteger;
@@ -12,11 +19,12 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static site.javen.solver.Utils.*;
 
 public class Main implements ByteDecoderHandler {
-    private static final File DATA_DIR = new File("/Users/coder/go/src/programmer_1024_2021/data");
+    private static final File DATA_DIR = new File("/Users/coder/go/src/programmer_1024_2021/data_generator/data");
 
     public static void main(String[] args) throws Exception {
         log("开始寻宝.....");
@@ -25,7 +33,7 @@ public class Main implements ByteDecoderHandler {
         }, 10);
     }
 
-    private final CopyOnWriteArrayList<String> matchMagics = new CopyOnWriteArrayList<>(new ArrayList<>(10000));
+    final AtomicInteger matchCount = new AtomicInteger(0);
 
     final ExecutorService ioService;
 
@@ -54,19 +62,62 @@ public class Main implements ByteDecoderHandler {
         for (Future<File> task : tasks) {
             task.get();
         }
+        log("匹配数:" + matchCount.get());
+        for (Future future : networkQueue) {
+            future.get();
+        }
         ioService.shutdownNow();
-        log("匹配数:" + matchMagics.size());
+        http2Default.initiateShutdown();
     }
 
 
     @Override
     public void onFoundItem(byte[] locationId, BigInteger locationValue, BigInteger magic) {
         if (isMatchMagic(locationValue, magic)) {
-            matchMagics.add(new String(locationId));
+            String loc = new String(locationId);
+            matchCount.incrementAndGet();
+            postResultToServer(loc);
         }
     }
 
 
+    private CopyOnWriteArrayList<Future> networkQueue = new CopyOnWriteArrayList<>(new ArrayList<>(10000));
+
+    CloseableHttpAsyncClient http2Default = HttpAsyncClients.createDefault();
+
+    {
+        http2Default.start();
+    }
+
+    /**
+     * 提交到服务器
+     *
+     * @param locationId
+     */
+    private void postResultToServer(String locationId) {
+        SimpleHttpRequest post = SimpleHttpRequest.create("POST", "http://localhost/dig");
+        post.setBody(String.format("{\n" +
+                "                \"token\":\"%s\",\n" +
+                "                \"locationid\":\"%s\"\n" +
+                "            }", "test2", locationId), ContentType.APPLICATION_JSON);
+        Future<SimpleHttpResponse> execute = http2Default.execute(post, new FutureCallback<>() {
+            @Override
+            public void completed(SimpleHttpResponse simpleHttpResponse) {
+            }
+
+            @Override
+            public void failed(Exception e) {
+                e.printStackTrace();
+            }
+
+
+            @Override
+            public void cancelled() {
+
+            }
+        });
+        networkQueue.add(execute);
+    }
 }
 
 
